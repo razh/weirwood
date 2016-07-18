d39aba7c713629fd427e5f0b8102c0d4b543062b
Raymond Zhou <somecrazyidiot@gmail.com>
2016-07-17 23:47:56 -0400

    Add basic render function

--- client/js/index.js
+++ client/js/index.js
@@ -16,3 +16,36 @@ class Body {
     this.angularVelocity = 0;
   }
 }
+
+
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+document.body.appendChild(canvas);
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const state = {
+  bodies: [],
+};
+
+function draw(ctx, state) {
+  ctx.lineWidth = 0.5;
+  ctx.strokeStyle = '#fff';
+
+  state.bodies.forEach(body => {
+    ctx.beginPath();
+    ctx.arc(body.position.x, body.position.y, 2, 0, 2 * Math.PI);
+    ctx.stroke();
+  });
+}
+
+for (let i = 0; i < 1000; i++) {
+  const body = new Body();
+  body.position.x = Math.random() * canvas.width;
+  body.position.y = Math.random() * canvas.height;
+
+  state.bodies.push(body);
+}
+
+draw(ctx, state);
